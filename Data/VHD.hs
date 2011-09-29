545379f89aeb70550184629cde69e785d2516515
Vincent Hanquez <vincent@snarc.org>
2011-09-29 15:29:07 +0100

    add a vhd create function. basic and incorrect for some fields.

--- Data/VHD.hs
+++ Data/VHD.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Data.VHD where
 
 import Control.Applicative
@@ -5,6 +6,7 @@ import Control.Exception
 import Control.Monad
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as B
+import Data.ByteString.Char8 ()
 import Data.Serialize
 import Data.VHD.Serialize
 import Data.VHD.Types
@@ -31,6 +33,52 @@ instance Serialize DynamicDiskInfo where
 
 getBat maxEntries = Bat <$> V.replicateM maxEntries getWord32be
 putBat (Bat ents) = V.mapM_ putWord32be ents
+
+create :: FilePath -> BlockSize -> Size -> IO ()
+create filePath bs virtualSize = do
+	BL.writeFile filePath $ runPutLazy $ do
+		put (DynamicDiskInfo footer header)
+		putBat bat
+		put footer
+	where
+		maxTableEntries = fromIntegral (virtualSize `divRoundUp` fromIntegral bs)
+		batSize         = maxTableEntries * 4 `roundUpToModulo` sectorLength
+		footerSize      = 512
+		headerSize      = 1024
+
+		bat = Bat (V.replicate (fromIntegral maxTableEntries) 0xffffffff)
+
+		footer = Footer
+			{ footerCookie             = cookie "conectix"
+			, footerIsTemporaryDisk    = False
+			, footerFormatVersion      = Version 1 0
+			, footerDataOffset         = 0 -- wrong
+			, footerTimeStamp          = 0 -- wrong
+			, footerCreatorApplication = creatorApplication "vs  "
+			, footerCreatorVersion     = Version 1 0
+			, footerCreatorHostOs      = CreatorHostOsWindows
+			, footerOriginalSize       = virtualSize
+			, footerCurrentSize        = virtualSize
+			, footerDiskGeometry       = DiskGeometry 1 1 1 -- c h s wrong
+			, footerDiskType           = DiskTypeDynamic
+			, footerCheckSum           = 0 -- wrong
+			, footerUniqueId           = randomUniqueId
+			, footerIsSavedState       = False
+			}
+		header = Header
+			{ headerCookie               = cookie "cxsparse"
+			, headerDataOffset           = 0xffffffff
+			, headerTableOffset          = footerSize + headerSize
+			, headerVersion              = Version 1 0
+			, headerMaxTableEntries      = maxTableEntries
+			, headerBlockSize            = bs
+			, headerCheckSum             = 0 -- wrong
+			, headerParentUniqueId       = randomUniqueId
+			, headerParentTimeStamp      = 0 -- wrong
+			, headerParentUnicodeName    = parentUnicodeName $ B.replicate 512 0
+			, headerParentLocatorEntries = parentLocatorEntries $ replicate 8 (ParentLocatorEntry $ B.replicate 24 0)
+			}
+		randomUniqueId = uniqueId $ B.replicate 16 0 -- wrong
 
 readDynamicDiskInfoFromFile :: FilePath -> IO (Either String DynamicDiskInfo)
 readDynamicDiskInfoFromFile f = return . decodeLazy =<< BL.readFile f
