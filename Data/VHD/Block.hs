f1ab67ad273ba523f6daf82e2140634e6dca65ca
Jonathan Knowles <jonathan.knowles@eu.citrix.com>
2011-10-06 12:29:35 +0100

    Revert earlier changes that renamed various pointer types from "X" to "XPtr". These types were intended to be opaque.

--- Data/VHD/Block.hs
+++ Data/VHD/Block.hs
@@ -1,12 +1,12 @@
 module Data.VHD.Block
-	( BlockPtr
-	, SectorPtr
+	( Block
+	, Sector
 	, withBlock
 	, writeBlock
 	, bitmapSizeOfBlock
 	, bitmapOfBlock
 	, sectorLength
-	, BitmapPtr (..)
+	, Bitmap (..)
 	, bitmapGet
 	, bitmapSet
 	, bitmapClear
@@ -30,9 +30,9 @@ import Control.Monad
 
 import System.IO.MMap
 
-data BlockPtr = BlockPtr BlockSize (Ptr Word8)
-data BitmapPtr = BitmapPtr (Ptr Word8)
-data SectorPtr = SectorPtr (Ptr Word8)
+data Block = Block BlockSize (Ptr Word8)
+data Bitmap = Bitmap (Ptr Word8)
+data Sector = Sector (Ptr Word8)
 
 sectorLength :: Word32
 sectorLength = 512
@@ -44,23 +44,23 @@ bitmapSizeOfBlock blockSize = fromIntegral ((nbSector `divRoundUp` 8) `roundUpTo
 
 -- | get a bitmap type out of a block type.
 -- the bitmap happens to be at the beginning of the block.
-bitmapOfBlock :: BlockPtr -> BitmapPtr
-bitmapOfBlock (BlockPtr _ ptr) = BitmapPtr ptr
+bitmapOfBlock :: Block -> Bitmap
+bitmapOfBlock (Block _ ptr) = Bitmap ptr
 
-dataOfBlock :: BlockPtr -> Ptr Word8
-dataOfBlock (BlockPtr bs ptr) = ptr `plusPtr` (bitmapSizeOfBlock bs)
+dataOfBlock :: Block -> Ptr Word8
+dataOfBlock (Block bs ptr) = ptr `plusPtr` (bitmapSizeOfBlock bs)
 
 -- | mmap a block using a filepath, a blocksize
-withBlock :: FilePath -> BlockSize -> Word32 -> (BlockPtr -> IO a) -> IO a
+withBlock :: FilePath -> BlockSize -> Word32 -> (Block -> IO a) -> IO a
 withBlock file bs sectorOff f = mmapWithFilePtr file ReadWrite (Just offsetSize) $ \(ptr, sz) ->
-		f (BlockPtr bs $ castPtr ptr)
+		f (Block bs $ castPtr ptr)
 	where
 		absoluteOffset = fromIntegral (fromIntegral sectorOff * sectorLength)
 		offsetSize     = (absoluteOffset, fromIntegral bs + fromIntegral bitmapSize)
 		nbSector       = (bs `divRoundUp` sectorLength)
 		bitmapSize     = (nbSector `divRoundUp` 8) `roundUpToModulo` sectorLength
 
-writeBlock :: BlockPtr -> ByteString -> Int -> IO ()
+writeBlock :: Block -> ByteString -> Int -> IO ()
 writeBlock block bs offset = do
 	-- sectors need to be prepared for differential disk if the bitmap was clear before,
 	-- at the moment assumption is it's 0ed
@@ -72,27 +72,26 @@ writeBlock block bs offset = do
 		sectorStart = fromIntegral offset `div` sectorLength
 		sectorEnd   = fromIntegral (offset + B.length bs) `div` sectorLength
 
-bitmapGet :: BitmapPtr -> Int -> IO Bool
-bitmapGet (BitmapPtr ptr) n = test `fmap` peekByteOff ptr offset
+bitmapGet :: Bitmap -> Int -> IO Bool
+bitmapGet (Bitmap ptr) n = test `fmap` peekByteOff ptr offset
 	where
 		test :: Word8 -> Bool
 		test = flip testBit (7-bit)
 		(offset, bit) = n `divMod` 8
 
-bitmapModify :: BitmapPtr -> Int -> (Int -> Word8 -> Word8) -> IO ()
-bitmapModify (BitmapPtr bptr) n f = peek ptr >>= poke ptr . f (7-bit)
+bitmapModify :: Bitmap -> Int -> (Int -> Word8 -> Word8) -> IO ()
+bitmapModify (Bitmap bptr) n f = peek ptr >>= poke ptr . f (7-bit)
 	where
 		ptr = bptr `plusPtr` offset
 		(offset, bit) = n `divMod` 8
 
-bitmapSet :: BitmapPtr -> Int -> IO ()
+bitmapSet :: Bitmap -> Int -> IO ()
 bitmapSet bitmap n = bitmapModify bitmap n (flip setBit)
 
--- FIXME use a faster way to set multiple bits when time permits
-bitmapSetRange :: BitmapPtr -> Int -> Int -> IO ()
+bitmapSetRange :: Bitmap -> Int -> Int -> IO ()
 bitmapSetRange bitmap start end
 	| start < end = bitmapSet bitmap start >> bitmapSetRange bitmap (start + 1) end
 	| otherwise   = return ()
 
-bitmapClear :: BitmapPtr -> Int -> IO ()
+bitmapClear :: Bitmap -> Int -> IO ()
 bitmapClear bitmap n = bitmapModify bitmap n (flip clearBit)
