e46a1fd0eba17691f40f4990ae628e4536dbac7f
Jonathan Knowles <jonathan.knowles@eu.citrix.com>
2011-11-03 16:03:25 +0000

    Tidy up imports and rearrange definitions within main executable.

--- Vhd.hs
+++ Vhd.hs
@@ -1,30 +1,60 @@
+import Control.Monad
+import qualified Data.ByteString as B
+import Data.Char
+import Data.IORef
 import Data.Vhd
-import Data.Vhd.Types
-import Data.Vhd.Block
-import Data.Vhd.Node
 import Data.Vhd.Bat
+import Data.Vhd.Block
 import Data.Vhd.Checksum
+import Data.Vhd.Node
+import Data.Vhd.Types
 import System.Environment (getArgs)
 import System.IO
 import Text.Printf
-import Control.Monad
-import Data.Char
 
-import Data.IORef
-
-import qualified Data.ByteString as B
-
-showBlockSize i
-	| i < 1024     = printf "%d bytes" i
-	| i < (1024^2) = printf "%d KiB" (i `div` 1024)
-	| i < (1024^3) = printf "%d MiB" (i `div` (1024^2))
-	| otherwise    = printf "%d GiB" (i `div` (1024^3))
+cmdConvert [fileRaw, fileVhd, size] = do
+	create fileVhd $ defaultCreateParameters
+		{ size = read size * 1024 * 1024
+		, useBatmap = True
+		}
+	withVhdNode fileVhd $ \node -> do
+		withFile fileRaw ReadMode $ \handle -> do
+			loop node handle 0
+	where
+		loop node handle offset = do
+			srcblock <- B.hGet handle (fromIntegral blockSize)
+			if B.null srcblock
+				then return ()
+				else do
+					unless (isBlockZero srcblock) $ do
+						let blockNb = offset `div` fromIntegral blockSize
+						appendEmptyBlock node blockNb
+						sectorOff <- batRead (nodeBat node) blockNb
+						withBlock (nodeFilePath node) (headerBlockSize $ nodeHeader node) sectorOff $ \block ->
+							writeDataRange block srcblock 0
+						putStrLn ("block " ++ show (offset `div` fromIntegral blockSize) ++ " written")
+					-- do something with block
+					loop node handle (offset + fromIntegral (B.length srcblock))
+		isBlockZero = B.all ((==) 0)
+		blockSize = 2 * 1024 * 1024
+cmdConvert _ = error "usage: convert <raw file> <vhd file> <size MiB>"
 
 cmdCreate [name, size] = create name $ defaultCreateParameters { size = (read size) * 1024 * 1024, useBatmap = True }
 cmdCreate _            = error "usage: create <name> <size MiB>"
 
-showChecksum checksum isValid =
-	printf "%08x (%s)" checksum (if isValid then "valid" else "invalid")
+cmdPropGet [file, key] = withVhdNode file $ \node -> do
+	case map toLower key of
+		"max-table-entries"   -> putStrLn $ show $ headerMaxTableEntries   $ nodeHeader node
+		"blocksize"           -> putStrLn $ show $ headerBlockSize         $ nodeHeader node
+		"disk-type"           -> putStrLn $ show $ footerDiskType          $ nodeFooter node
+		"current-size"        -> putStrLn $ show $ footerCurrentSize       $ nodeFooter node
+		"uuid"                -> putStrLn $ show $ footerUniqueId          $ nodeFooter node
+		"parent-uuid"         -> putStrLn $ show $ headerParentUniqueId    $ nodeHeader node
+		"parent-timestamp"    -> putStrLn $ show $ headerParentTimeStamp   $ nodeHeader node
+		"parent-filepath"     -> putStrLn $ show $ headerParentUnicodeName $ nodeHeader node
+		"timestamp"           -> putStrLn $ show $ footerTimeStamp         $ nodeFooter node
+		_                     -> error "unknown key"
+cmdPropGet _ = error "usage: prop-get <file> <key>"
 
 cmdRead [file] = withVhdNode file $ \node -> do
 	let hdr = nodeHeader node
@@ -55,53 +85,19 @@ cmdRead [file] = withVhdNode file $ \node -> do
 	putStrLn ("blocks allocated  : " ++ show nb ++ "/" ++ show (headerMaxTableEntries hdr))
 cmdRead _ = error "usage: read <file>"
 
-cmdPropGet [file, key] = withVhdNode file $ \node -> do
-	case map toLower key of
-		"max-table-entries"   -> putStrLn $ show $ headerMaxTableEntries   $ nodeHeader node
-		"blocksize"           -> putStrLn $ show $ headerBlockSize         $ nodeHeader node
-		"disk-type"           -> putStrLn $ show $ footerDiskType          $ nodeFooter node
-		"current-size"        -> putStrLn $ show $ footerCurrentSize       $ nodeFooter node
-		"uuid"                -> putStrLn $ show $ footerUniqueId          $ nodeFooter node
-		"parent-uuid"         -> putStrLn $ show $ headerParentUniqueId    $ nodeHeader node
-		"parent-timestamp"    -> putStrLn $ show $ headerParentTimeStamp   $ nodeHeader node
-		"parent-filepath"     -> putStrLn $ show $ headerParentUnicodeName $ nodeHeader node
-		"timestamp"           -> putStrLn $ show $ footerTimeStamp         $ nodeFooter node
-		_                     -> error "unknown key"
-cmdPropGet _ = error "usage: prop-get <file> <key>"
+showBlockSize i
+	| i < 1024     = printf "%d bytes" i
+	| i < (1024^2) = printf "%d KiB" (i `div` 1024)
+	| i < (1024^3) = printf "%d MiB" (i `div` (1024^2))
+	| otherwise    = printf "%d GiB" (i `div` (1024^3))
 
-cmdConvert [fileRaw, fileVhd, size] = do
-	create fileVhd $ defaultCreateParameters
-		{ size = read size * 1024 * 1024
-		, useBatmap = True
-		}
-	withVhdNode fileVhd $ \node -> do
-		withFile fileRaw ReadMode $ \handle -> do
-			loop node handle 0
-	where
-		loop node handle offset = do
-			srcblock <- B.hGet handle (fromIntegral blockSize)
-			if B.null srcblock
-				then return ()
-				else do
-					unless (isBlockZero srcblock) $ do
-						let blockNb = offset `div` fromIntegral blockSize
-						appendEmptyBlock node blockNb
-						sectorOff <- batRead (nodeBat node) blockNb
-						withBlock (nodeFilePath node) (headerBlockSize $ nodeHeader node) sectorOff $ \block ->
-							writeDataRange block srcblock 0
-						putStrLn ("block " ++ show (offset `div` fromIntegral blockSize) ++ " written")
-					-- do something with block
-					loop node handle (offset + fromIntegral (B.length srcblock))
-
-
-		isBlockZero = B.all ((==) 0)
-		blockSize = 2 * 1024 * 1024
-cmdConvert _ = error "usage: convert <raw file> <vhd file> <size MiB>"
+showChecksum checksum isValid =
+	printf "%08x (%s)" checksum (if isValid then "valid" else "invalid")
 
 main = do
 	args <- getArgs
 	case args of
-		"create"  :xs -> cmdCreate  xs
-		"read"    :xs -> cmdRead    xs
 		"convert" :xs -> cmdConvert xs
+		"create"  :xs -> cmdCreate  xs
 		"prop-get":xs -> cmdPropGet xs
+		"read"    :xs -> cmdRead    xs
